b8dd435685fba26355d96cb18fe1fe6e3f0f9b17
Gnieark <rpasserieu@ch-boispetit.fr>
2016-06-13 10:02:22 +0200

    empty file connectfour

---
+++ connectfour.php
@@ -0,0 +1,23 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST'); 
+header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
+
+/*
+* stupid IA for battle ship
+* choose by random a free column
+*/
+$in=file_get_contents('php://input');
+$params=json_decode($in, TRUE);
+switch($params['action']){
+	case "init":
+		echo '{"name":"Gnieark"}';
+		break;
+	case "play-turn":
+
+		$grid=$params['board'];
+
+		break;
+	default:
+		break;
+}
