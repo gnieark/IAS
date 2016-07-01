80c98f5f7b1b44bba7166c2dae31ac9d0967ed79
Gnieark <gnieark@free.fr>
2016-07-01 07:34:27 +0200

    stupid tron init message response

--- stupidIATron.php
+++ stupidIATron.php
@@ -1,5 +1,5 @@
 <?php
-<?php
+
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST'); 
 header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
